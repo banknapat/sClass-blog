014bf02898aea8fef51fb53567c6df0ac2221c8b
banknapat <banknapat20@gmail.com>
2021-01-23 21:26:19 +0700

    add blog-detail.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,22 @@ $(window).scroll(function() {
         $('#navbar').css('padding', '25px')
     }
 })
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        nav:false,
+        dots:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+            },
+            1000:{
+                items:3,
+            }
+        }
+    });
+  });
